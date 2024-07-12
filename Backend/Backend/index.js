7c9f6b4cea7d071ef7c99aa1a74c030144a2bdf5
ManishJoshi <joshi8763@gmail.com>
2024-07-12 20:20:32 +0530

    App created

--- Backend/Backend/index.js
+++ Backend/Backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
@@ -30,6 +31,15 @@ try {
 // defining routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
+
+//depolyment code
+if (process.env.NODE_ENV === "production") {
+  const dirPath = Path.resolve(); //will store path of current working directory
+  app.use(express.static("Frontend/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirPath, "Frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
